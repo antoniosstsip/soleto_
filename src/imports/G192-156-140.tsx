2bcf3c431cd9ed87b296b45ef179b2edef799b8f
antoniosstsip <antonistsipoulakos23@gmail.com>
2026-03-06 21:41:11 +0200

    Add files via upload

---
+++ src/imports/G192-156-140.tsx
@@ -0,0 +1,43 @@
+import svgPaths from "./svg-u68r72h2ep";
+
+function G2() {
+  return (
+    <div className="absolute contents inset-0" data-name="g200">
+      <svg className="absolute block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 731.407 268.021">
+        <g id="g202">
+          <path d={svgPaths.p34c92080} fill="url(#paint0_linear_156_144)" id="path226" />
+        </g>
+        <defs>
+          <linearGradient gradientUnits="userSpaceOnUse" id="paint0_linear_156_144" x1="-0.0005925" x2="731.407" y1="134.011" y2="134.011">
+            <stop stopColor="#BC7725" />
+            <stop offset="0.060606" stopColor="#A86426" />
+            <stop offset="0.139394" stopColor="#B78137" />
+            <stop offset="0.278788" stopColor="#C58421" />
+            <stop offset="0.442424" stopColor="#F8DA57" />
+            <stop offset="0.563636" stopColor="#F2D34E" />
+            <stop offset="0.618182" stopColor="#F8DA57" />
+            <stop offset="0.763636" stopColor="#D7941D" />
+            <stop offset="0.878788" stopColor="#D8AB42" />
+            <stop offset="1" stopColor="#99532B" />
+          </linearGradient>
+        </defs>
+      </svg>
+    </div>
+  );
+}
+
+function G1() {
+  return (
+    <div className="absolute contents inset-0" data-name="g194">
+      <G2 />
+    </div>
+  );
+}
+
+export default function G() {
+  return (
+    <div className="relative size-full" data-name="g192">
+      <G1 />
+    </div>
+  );
+}
---
+++ src/imports/G228-40-1790.tsx
@@ -0,0 +1,30 @@
+import svgPaths from "./svg-tev8xzpsnw";
+
+export default function DotsComponent() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 186 32"
+      fill="none"
+      style={{ width: '100%', height: '100%' }}
+    >
+      <path
+        d={svgPaths.p51da00}
+        fill="url(#paint0_linear_228_1790)"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear_228_1790"
+          x1="92.768"
+          y1="0"
+          x2="92.768"
+          y2="31.7228"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#E3C470" />
+          <stop offset="1" stopColor="#DDB64B" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+}
